db5fdb4f64321d9d5623f7bc24676812dc456444
PhenomenalTechs-hub <71147108+PhenomenalTechs-hub@users.noreply.github.com>
2021-06-12 08:52:13 +0530

    Removed the Product Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,9 +63,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
@@ -76,12 +73,12 @@ const Navbar = class extends React.Component {
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://www.youtube.com/channel/UC0-rBSZd0-LiGmOySaBgiQQ"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <span className="icon">
-                  <img src={github} alt="Github" />
+                  <button>Subscribe</button>
                 </span>
               </a>
             </div>
