bebaf21d9614f0471ef68f89d1f2404e5825830d
PhenomenalTechs-hub <phenomenaltechs6080@gmail.com>
2021-06-12 12:00:22 +0530

    removed helmet

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,23 +1,14 @@
 import React from "react";
-import { Helmet } from "react-helmet";
+
 import Layout from "../../components/Layout";
 import BlogRoll from "../../components/BlogRoll";
 
-export const BlogIndexPage = () => {
-  return (
-    <Layout>
-      <div
-        className="full-width-image-container margin-top-0"
-        style={{
-          backgroundImage: `url('/img/blog-index.jpg')`,
-        }}
-      >
-        <Helmet>
-          <title>Blogs</title>
-          <meta name="description" content="App Description" />
-        </Helmet>
-        <h1
-          className="has-text-weight-bold is-size-1"
+export default class BlogIndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
           style={{
             boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
             backgroundColor: "#f40",
@@ -25,16 +16,26 @@ export const BlogIndexPage = () => {
             padding: "1rem",
           }}
         >
-          Latest Stories
-        </h1>
-      </div>
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <BlogRoll />
-          </div>
+          <h1
+            className="has-text-weight-bold is-size-1"
+            style={{
+              boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+              backgroundColor: "#f40",
+              color: "white",
+              padding: "1rem",
+            }}
+          >
+            Latest Stories
+          </h1>
         </div>
-      </section>
-    </Layout>
-  );
-};
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <BlogRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
